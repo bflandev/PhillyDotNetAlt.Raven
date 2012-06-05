2a4f5d25bce4ed1a05d7b481bb0de881faa78575
= <=>
2012-06-05 12:48:36 -0400

    example1

--- PhillyDotNetAlt.Raven/Program.cs
+++ PhillyDotNetAlt.Raven/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using PhillyDotNetAlt.Raven.Domain.Model;
 using Raven.Client.Document;
 
 namespace PhillyDotNetAlt.Raven
@@ -14,7 +15,9 @@ namespace PhillyDotNetAlt.Raven
             {
                 using (var session = store.OpenSession())
                 {
-                    
+                    var post = new Post {Id = 1, By = "Brian", Content = "RavenDb is cool", Hates = 100};
+                    session.Store(post);
+                    session.SaveChanges();
                 }
             }
         }
